68fd8ea486f78d9071f970d92978f379140e9aac
Lucas Rosa <lucasr.barbosa@fornecedores.sicoob.com.br>
2024-09-30 15:42:02 -0300

    Subindo mvp

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: false
+});
+
+module.exports = User;
